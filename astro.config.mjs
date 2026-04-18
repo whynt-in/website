07435eeda663dfdf4ddc448f332ce5a2f5dadd2b
Gautham Vinayachandran <vinayachandran@aol.com>
2026-04-18 19:27:01 +0530

    Cleared repo for Astro template

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    }
-});
