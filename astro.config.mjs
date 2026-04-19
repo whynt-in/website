64bf385bd5e90752fc16e3dd94b968bb662711a9
Gautham Vinayachandran <vinayachandran@aol.com>
2026-04-19 19:01:26 +0530

    Added Cloudflare Pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://whynt.in/",
   integrations: [icon(), sitemap()],
+  adapter: cloudflare(),
 });
