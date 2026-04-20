ad7bf1b227e7cf041fce1a9752dcd873ab7b50b4
Gautham Vinayachandran <vinayachandran@aol.com>
2026-04-20 10:22:10 +0530

    Added Google Analytics and TagManager

--- src/config/analytics.ts
+++ src/config/analytics.ts
@@ -4,5 +4,5 @@
 // - Google Tag Manager ID: for Google Tag Manager
 
 export const googleSiteVerification = ''
-export const googleAnalyticsMeasurementID = import.meta.env.PUBLIC_GA_TRACKING_ID // Replace with your Google Analytics Measurement ID (e.g. "G-XXXXXXXXXX")
-export const googleTagManagerID = ''
+export const googleAnalyticsMeasurementID = import.meta.env.PUBLIC_GA_TRACKING_ID
+export const googleTagManagerID = import.meta.env.PUBLIC_GTM_ID
