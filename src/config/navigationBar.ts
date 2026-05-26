831a7d3a954ad6cd10d2a1b1fe16da2616765520
Gautham Vinayachandran <vinayachandran@aol.com>
2026-05-26 22:44:08 +0530

    Improved CTA in some pages

--- src/config/navigationBar.ts
+++ src/config/navigationBar.ts
@@ -53,5 +53,5 @@ export const navigationBarData: NavData = {
 		},
 		{ name: 'Contact', link: '/contact' }
 	],
-	navActions: [{ name: 'Try it now', link: '/contact', style: 'primary', size: 'lg' }]
+	navActions: [{ name: 'Get Started', link: '/contact', style: 'primary', size: 'lg' }]
 }
